14bb33beea2aef27da1c62c753edadc0ea906555
Bailey Keefer <bnk423@gmail.com>
2020-04-08 15:32:27 -0400

    finished binarySearch test specs

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,30 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	//base cases - 2
+	if(array.length === 0){
+		return false;
+	}
+	if(array.length === 1){
+		if(array[0]=== target){
+			return true;
+		} else{
+			return false;
+		}
+	}
+	//not base case
+	let mid = Math.floor(array.length/2);
+	if(target < array[mid]){
+		let leftHalf = array.slice(0, mid);
+		return binarySearch(leftHalf, target);
+	}
+	else if(target === array[mid]){
+		return true;
+	}
+	else{
+		let rightHalf = array.slice(mid);
+		return binarySearch(rightHalf, target);
+	}
 };
 
 /*
@@ -15,3 +38,13 @@ const binarySearch = (array, target) => {
 */
 
 module.exports = binarySearch
+
+//NOTES
+// take given array & see if the middle point is < , > = the target
+// if target < midpoint --> looking at only the left half of original array
+// keep dividing array in this manner until you have 1 element
+// not base case (recursive)--> call the function on the halved side we're still focusing on
+// base cases --> 1.) is the array empty? --> return false
+//                2.) is this one element === target?
+//                     if yes --> return true, if no---> return false
+// [ 1 , 4] arr.length = 2 , mid = 1
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -5,7 +5,7 @@ const binarySearch = require('./binary-search');
 const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
-
+const test4 = [];
 
 describe('Binary Search', () => {
 	it('returns true if target value is in array', () =>{
@@ -20,6 +20,9 @@ describe('Binary Search', () => {
 		expect(binarySearch(test3, 4)).to.equal(false);
 	});
 
+	it('returns false for an empty array', () => {
+		expect(binarySearch(test4, 25)).to.equal(false);
+	});
 	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
 
 });
